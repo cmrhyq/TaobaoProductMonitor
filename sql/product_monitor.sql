adb390046290f57676a033918a6f548cac8c0fa0
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-04-25 21:39:03 +0800

    1、新增初始化数据库脚本

--- sql/product_monitor.sql
+++ sql/product_monitor.sql
@@ -51,4 +51,8 @@ CREATE TABLE `products`  (
   PRIMARY KEY (`product_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
+insert into store.products (product_id, user_id, platform, product_url, product_name, product_tk, monitor_status, notify_email, gmt_create, gmt_modified)
+values  (19, 1, '淘宝', 'https://m.tb.cn/h.5zxyK10h65Gy9AU?tk=X2X2WKIqj7', '优衣库女装麻混纺吊带连衣裙(打褶高腰时尚法式轻盈新款)466540', 'X2X2WKIqj7o ', 11, 'cmrhyq@163.com', '2024-04-18 00:43:20', '2024-04-25 21:18:06'),
+        (20, 1, '淘宝', 'https://m.tb.cn/h.5zxz3j69cN5go4k?tk=XUymWKIqxIz', '优衣库女装网眼V领短针织开衫长袖薄外套空调衫2024新款468541', 'XUymWKIqxIz', 11, 'cmrhyq@163.com', '2024-04-18 00:43:20', '2024-04-25 21:18:06');
+
 commit;
