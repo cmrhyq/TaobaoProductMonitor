5bb2d231afafa811ffea4c2ed8275960687531ac
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2025-04-11 21:17:25 +0800

    删除不必要的配置

--- config/read_conf.py
+++ config/read_conf.py
@@ -17,14 +17,13 @@
 @Description None
 """
 import configparser
-from system.conf import cm
+from config.manager_config import cm
 
 
 class ReadConfig(object):
     """
     配置文件
     """
-
     def __init__(self):
         self.config = configparser.RawConfigParser()  # 当有%的符号时请使用Raw读取
         self.config.read(cm.ini_file, encoding='utf-8')
@@ -51,14 +50,6 @@ class ReadConfig(object):
             self.config.write(f)
 
     @property
-    def web_chrome_driver_path(self):
-        return self._get('web', 'chrome_driver_path')
-
-    @property
-    def web_edge_driver_path(self):
-        return self._get('web', 'edge_driver_path')
-
-    @property
     def taobao_cron(self):
         return self._get('taobao', 'cron')
 
@@ -75,33 +66,9 @@ class ReadConfig(object):
         return self._get('mail', 'license')
 
     @property
-    def database_host(self):
-        return self._get('database', 'host')
-
-    @property
-    def database_port(self):
-        return self._get('database', 'port')
-
-    @property
-    def database_username(self):
-        return self._get('database', 'username')
-
-    @property
-    def database_password(self):
-        return self._get('database', 'password')
-
-    @property
-    def database_db(self):
-        return self._get('database', 'db')
-
-    @property
     def log_tag(self):
         return self._get('log', 'tag')
 
     @property
     def log_save_path(self):
         return self._get('log', 'save_path')
-
-    @property
-    def log_exist_counts(self):
-        return self._get('log', 'exist_counts')
